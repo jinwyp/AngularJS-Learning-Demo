e66e498b6636e8f7c5220e854d6eaffea8104322
Jin <jinwyp@gmail.com>
2015-05-22 18:00:40 +0800

    add gulp

--- README.md
+++ README.md
@@ -2,28 +2,34 @@
 
 
 ## 第一步 开始一个项目
+
+### 安装Express框架
 先确保系统已经安装好 [NodeJS](https://nodejs.org/)
 
 然后在命令行运行以下命令安装[Express](http://expressjs.com/) (Express 是基于Nodejs做网站的一个开发框架)
 
-同时安装express 命令行工具 [express-generator](http://expressjs.com/starter/generator.html)
     npm install express --save
+
+同时安装express 命令行工具 [express-generator](http://expressjs.com/starter/generator.html)
 
     npm install express-generator --save-dev -g
 
-注意: 在MAC系统下 可能需要管理员权限安装 请使用 sudo npm install express --save  命令
+注意: 在MAC系统下 可能需要管理员权限安装 请在原命令前增加sudo
+
+    sudo npm install express --save
 
 
+### 创建一个项目
 
 然后 创建一个项目 名字叫app
 
-    express app -e -c compass
+    express app -e 或 express app -e -c compass
 
 参数 -e 是使用ejs模板引擎, -c compass 是使用compass sass CSS预处理
 
-创建完毕会在当前文件夹下增加一个app文件夹
+创建完毕后, 在当前文件夹下会增加一个app文件夹
 
-注意: 项目所需要的后端依赖的库都在 node_modules 下。后端文件和库使用npm 命令安装，package.json 里面记录所有信息。
+注意: 项目所需要的后端依赖的库都在 node_modules 下。后端文件和库使用npm install 命令安装。在使用 npm install --save 命令会自动把信息保存在 package.json 里面。
 
 
 ## 第二步 使用Bower 构建前端基础结构
@@ -34,27 +40,48 @@
 
 安装前端所需要依赖的库
 
-bower install angular --save
-bower install jquery --save
-bower install bootstrap --save
-bower install reveal.js --save
+    bower install angular --save
+    bower install jquery --save
+    bower install bootstrap --save
+    bower install reveal.js --save
 
 注意: 项目所需要的前端依赖的库都在 bower_components 下。 .bowerrc文件记录着文件存放的路径默认是bower_components
 
 
 ## 第三步 运行网站
 
-进入app文件夹，键入 DEBUG=myapp npm start 运行网站  然后在浏览器 打开http://localhost:8080/ 就可以访问了
+进入app文件夹，键入 DEBUG=myapp npm start 运行网站  然后在浏览器 打开http://localhost:8080/ 就可以访问了。
 
 
-## 第四步 使用Gulp 来运行网站
+## 第四步 使用Gulp 批处理任务工具 来运行网站
 
-安装Gulp
+Gulp可以处理一系列的工作, 例如编译Sass为css,压缩js代码,合并js和css文件, 最后启动网站。
 
-    npm install -g gulp
+### 安装Gulp
 
-gulp-nodemon 是重启服务器的插件。
-gulp-livereload 后端控制前端同步刷新，可能需要安装的 chrome 浏览器插件 LiveReload(也可以不用浏览器插件)
-gulp-livereload [浏览器插件](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei)
-[livereload-js](https://github.com/livereload/livereload-js)
+    npm install -g gulp --save-dev
+
+gulp-nodemon 是重启服务器的插件。 因为我们修改后端的nodejs代码, 需要人工重启服务器才能看到更新, 使用nodemon就可以自动监视文件变化重启服务器。
+
+gulp-livereload 是自动刷新前端页面的插件, 这样修改了css不用手动刷新页面就能看到最新的修改, 而且可以在电脑,手机上同步刷新。
+
+需要安装的 chrome 浏览器插件 LiveReload(也可以不用浏览器插件) [浏览器插件](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei)
+或不用浏览器插件 在页面中插入以下代码 [livereload-js](https://github.com/livereload/livereload-js)
+
+    <script type="text/javascript">
+        document.write('<script src="' + (location.protocol || 'http:') + '//' + (location.hostname || 'localhost') + ':35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
+    </script>
+
+
+### 编写Gulp 任务
+
+具体请查看gulpfile.js内容, 默认gulp会读取gulpfile.js的任务
+
+### 运行Gulp 任务
+
+在命令行运行以下命令启动网站, 打开http://localhost:8080/ 就可以访问了。
+
+    gulp 或 gulp default
+
+
 
--- app/gulpfile.js
+++ app/gulpfile.js
@@ -9,19 +9,36 @@
 var gulp       = require('gulp');
 var nodemon    = require('gulp-nodemon');
 var livereload = require('gulp-livereload');
+
 var jshint = require('gulp-jshint');
 var jshintReporter = require('jshint-stylish');
+
 var compass = require('gulp-compass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
+
+var uglifyJS = require('gulp-uglify');
+var minifyHtml = require("gulp-minify-html");
+
+var ngTemplateCache = require('gulp-angular-templatecache');
+var ngAnnotate = require('gulp-ng-annotate');
+
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var inject = require('gulp-inject');
+
+
+
 
 // 文件的路径
 var paths = {
     base: 'public/',
     client: {
-        js : ['public/angulardemo/**/*.js', 'public/angularslide/**/*.js'],
-        sass : ['public/angulardemo/css/sass/*'],
-        css : ['public/angulardemo/css/stylesheets/*']
+        js : ['public/angulardemo/js/**/*.js'],
+        //js2 : ['public/angulardemo/js/**/*.js', 'public/angularslide/js/**/*.js'],
+        sass : ['public/angulardemo/css/sass/**/*.scss'],
+        css : ['public/angulardemo/css/stylesheets/*.css'],
+        dist : 'public/angulardemo/dist'
     },
     server: {
         index: 'bin/www',
@@ -54,28 +71,13 @@ var nodemonConfig = {
 
 
 
-/********************  监视JS文件的变化 并用jshint 检查语法 注: jshint 可能会伤害你的感情  ********************/
-gulp.task('jshint',function(){
-    return gulp.src(paths.client.js)
-    .pipe(jshint())
-    .pipe(jshint.reporter(jshintReporter));
-});
-
-
-/********************  使用nodemon 自动重启服务器  ********************/
-gulp.task('nodemon', function() {
-    return nodemon(nodemonConfig);
-});
-
-
-/********************  当客户端被监听的文件改变时，刷新浏览器  ********************/
+/********************  监视Sass文件变化 生成CSS样式文件  ********************/
 gulp.task('compass', function() {
 
     return gulp.src(paths.client.sass)
     .pipe(compass(compassConfig))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(gulp.dest(compassConfig.css))
-    .pipe(livereload());
+    .pipe(gulp.dest(compassConfig.css));
 
     //var server = livereload();
     //return gulp.watch(paths.client.css, function(event) {
@@ -84,10 +86,58 @@ gulp.task('compass', function() {
 });
 
 
+
+/********************  监视CSS文件变化 压缩CSS样式文件  ********************/
+gulp.task('minifycss', function() {
+
+    return gulp.src(paths.client.css)
+    .pipe(concat('main.css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss(
+        {
+            keepBreaks:false,
+            keepSpecialComments:0 //* for keeping all (default), 1 for keeping first one only, 0 for removing all
+        }
+    ))
+    .pipe(gulp.dest(paths.client.dist))
+    .pipe(livereload());
+});
+
+
+
+/********************  监视JS文件的变化 并用jshint 检查语法 注: jshint 可能会伤害你的感情  ********************/
+gulp.task('jshint',function(){
+    return gulp.src(paths.client.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter(jshintReporter));
+});
+
+
+
+/********************  监视JS文件的变化 Minify JavaScript with UglifyJS2  ********************/
+gulp.task('minifyjs', ['jshint'], function(){
+    gulp.src(paths.client.js)
+    .pipe(ngAnnotate())
+    .pipe(concat('app.min.js'))
+    .pipe(uglifyJS())
+    .pipe(gulp.dest(paths.client.dist))
+});
+
+
+
+/********************  当客户端被监听的文件改变时，刷新浏览器  ********************/
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch(paths.client.js, ['jshint']);
+    gulp.watch(paths.client.js, ['minifyjs']);
     gulp.watch(paths.client.sass, ['compass']);
+    gulp.watch(paths.client.css, ['minifycss']);
+});
+
+
+
+/********************  使用nodemon 自动重启服务器  ********************/
+gulp.task('nodemon', function() {
+    return nodemon(nodemonConfig);
 });
 
 
@@ -95,5 +145,8 @@ gulp.task('watch', function() {
 
 
 
+
+
+
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['compass', 'nodemon', 'watch']);
+gulp.task('default', ['minifyjs', 'watch', 'compass', 'nodemon' ]);
