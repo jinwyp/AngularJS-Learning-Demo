6b30d5b3ae312fdb7248c844d7d82fa0ff287d9c
JinWYP <jinwyp@gmail.com>
2016-03-18 16:15:39 +0800

    update

---
+++ app/models/post/post.js
@@ -0,0 +1,65 @@
+/**
+ * Created by jinwyp on 3/18/2016.
+ */
+
+
+/**
+ * Module dependencies
+ */
+
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+
+
+/**
+ * Mongoose schema
+ */
+
+
+var PostSchema = new Schema({
+    title:  String,
+    postdate: {type: Date, default: Date.now},
+    author: {type: String}
+});
+
+
+
+
+
+/**
+ * Mongoose plugin
+ */
+
+
+
+
+
+
+
+
+
+/**
+ * Add your
+ * - pre-save hooks
+ * - validations
+ * - virtuals
+ */
+
+
+
+
+
+
+/**
+ * Mongoose Schema Statics
+ *
+ * http://mongoosejs.com/docs/guide.html
+ *
+ */
+
+
+
+module.exports = mongoose.model('Post', PostSchema);
