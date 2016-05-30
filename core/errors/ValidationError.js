8d9390920ed6b9869096200f027d7e28636bcf0b
JinWYP <jinwyp@gmail.com>
2016-05-30 18:06:07 +0800

    update

--- core/errors/ValidationError.js
+++ core/errors/ValidationError.js
@@ -1,5 +1,3 @@
-var validationCode = require('./ValidationCode.js');
-
 
 function ValidationError(code, message, field) {
     Error.captureStackTrace(this, this.constructor);
@@ -15,6 +13,41 @@ function ValidationError(code, message, field) {
 
 ValidationError.prototype = Object.create(Error.prototype);
 ValidationError.prototype.constructor = ValidationError;
+
+
+
+
+var validationCode = {
+    user : {
+        usernameWrong : 1001,
+        passwordWrong : 1002,
+        emailWrong : 1003,
+        mobileWrong : 1004,
+
+        usernameExist : 1011,
+        passwordExist : 1012,
+        emailExist : 1013,
+        mobileExist : 1014,
+
+        usernameNotFound : 1101,
+        passwordNotMatch : 1102,
+
+        messageTypeWrong : 1201,
+        SMSCodeLengthWrong : 1203,
+        SMSCodeNotFound : 1204,
+        SMSCodeExpired : 1205
+    },
+
+    token : {
+        tokenNotFound : 4001,
+        userNotFound : 4002,
+        tokenDecodeWrong : 4005,
+        tokenExpired : 4007,
+        tokenLengthWrong : 4008
+
+    }
+};
+
 
 ValidationError.code = validationCode;
 
